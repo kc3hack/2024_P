8bac0ae7cd9bc4227ff4a310ca695f6f9d65d3b6
aqua5432 <93812121+aqua5432@users.noreply.github.com>
2024-02-24 00:00:49 +0900

    [fix]Playerスクリプトの編集・カメラ揺れの実装

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public GameObject hitObject;
+    private GameObject hitObject;
+    private bool hasIncursion,hasPassed,hasBack,hasReached;
+    public GameObject IncursionObject,PassObject,BackObject,ReachObject;
     
     void Update()
     {
+        Ray ray = new Ray(transform.forward, Vector3.down);
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+
+        if (Physics.Raycast(ray, out hit))
         {
             hitObject = hit.collider.gameObject;
-            bool WorldBool = hitObject.GetComponent<IWorldBool>().GetWorldBool();
+            if(hitObject ==IncursionObject)
+            {
+                hasIncursion = hitObject.GetComponent<IWorldBool>().GetWorldBool();
+            }
+            else if(hitObject ==PassObject)
+            {
+                hasPassed = hitObject.GetComponent<IWorldBool>().GetWorldBool();
+            }
+            else if(hitObject ==BackObject)
+            {
+                hasBack = hitObject.GetComponent<IWorldBool>().GetWorldBool();
+            }
+            else if(hitObject ==ReachObject)
+            {
+                hasReached = hitObject.GetComponent<IWorldBool>().GetWorldBool();
+            }
         }
     }
 }

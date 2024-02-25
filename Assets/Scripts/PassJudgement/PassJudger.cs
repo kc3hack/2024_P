57897fc55cf3c9ddfe5a3aa5e78b7a31c5e43397
hikaru0331 <90139340+hikaru0331@users.noreply.github.com>
2024-02-25 12:02:44 +0900

    [add]ワールド生成座標の調整(途中)

--- Assets/Scripts/PassJudgement/PassJudger.cs
+++ Assets/Scripts/PassJudgement/PassJudger.cs
@@ -14,13 +14,13 @@ public class PassJudger : MonoBehaviour, IPassJudgement
 
     Vector3 IPassJudgement.ReturnGeneratePosition()
     {
-        generatePosition = this.transform.position + (new Vector3(-122.72f, 9.4f, -194.56f) - new Vector3(-110.25f, 11.61f, -126.52f));
+        generatePosition = this.transform.position + (new Vector3(-122.8f, 12.85f, -202.22f) - new Vector3(-110.25f, 11.61f, -126.52f));
         return generatePosition;
     }
 
     Vector3 IPassJudgement.ReturnGenerateRotation()
     {        
-        generateRotation = this.transform.rotation.eulerAngles + (new Vector3(-89.98f, 0f, 0f) - new Vector3(0f, -90f, 0f));
+        generateRotation = this.transform.rotation.eulerAngles - new Vector3(0f, 90f, 0f);
         return generateRotation;
     }
 }
